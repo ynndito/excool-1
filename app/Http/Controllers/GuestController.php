1bb5f44f07ab14dcf6d9a6388e220778a72728b5
ilhamsahid <ilhamsahit988@gmail.com>
2025-07-25 13:38:14 +0700

    refactor: mengubah kode guest jadi bersih

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Ekskul;
-use App\Models\Pengumuman;
-use Illuminate\Http\Request;
+use App\Services\EkskulService;
+use App\Services\PengumumanService;
 
 class GuestController extends Controller
 {
+    protected EkskulService $ekskulService;
+    protected PengumumanService $pengumumanService;
+
+    public function __construct(EkskulService $ekskulService, PengumumanService $pengumumanService){
+        $this->ekskulService = $ekskulService;
+        $this->pengumumanService = $pengumumanService;
+    }
+
     public function index(){
-        $ekskuls = Ekskul::with('pembina')->withCount('siswa')->get();
-        $announcements = Pengumuman::with('ekskul')->get();
+        $ekskuls = $this->ekskulService->getAllWithCount();
+        $announcements = $this->pengumumanService->getAll();
         return view('guest.dashboard.index', compact('ekskuls', 'announcements'));
     }
 }
