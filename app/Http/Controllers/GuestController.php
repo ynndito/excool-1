36748f205a0b3484fab12b7a9b8cd1923c25d3ee
ilhamsahid <ilhamsahit988@gmail.com>
2025-07-28 20:15:13 +0700

    feat: memasukkan recent activities ke dalam guest controller

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -4,20 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Services\EkskulService;
 use App\Services\PengumumanService;
+use App\Services\RecentActivitiesService;
 
 class GuestController extends Controller
 {
     protected EkskulService $ekskulService;
     protected PengumumanService $pengumumanService;
+    protected RecentActivitiesService $recentActivitiesService;
 
-    public function __construct(EkskulService $ekskulService, PengumumanService $pengumumanService){
+    public function __construct(EkskulService $ekskulService, PengumumanService $pengumumanService, RecentActivitiesService $recentActivitiesService){
         $this->ekskulService = $ekskulService;
         $this->pengumumanService = $pengumumanService;
+        $this->recentActivitiesService = $recentActivitiesService;
     }
 
     public function index(){
         $ekskuls = $this->ekskulService->getAllWithCount();
         $announcements = $this->pengumumanService->getAll();
-        return view('guest.dashboard.index', compact('ekskuls', 'announcements'));
+        $recentActivities = $this->recentActivitiesService->getAll();
+        return view('guest.dashboard.index', compact('ekskuls', 'announcements', 'recentActivities'));
     }
 }
