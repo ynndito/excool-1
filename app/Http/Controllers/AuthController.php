0e5170175ceadd1dc4020e97a1eed416860db817
ilhamsahid <ilhamsahit988@gmail.com>
2025-07-30 11:00:11 +0700

    feat: membuat logika agar bisa login dan logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+            return response()->json(['status' => 'success', 'message' => 'Login berhasil', 'user' => Auth::user()]);
+        }
+
+        return response()->json(['status' => 'error', 'message' => 'Email atau password salah'], 401);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+    }
+}
